41562ae4a673cbabce55cb78c6dce636674e941f
Saul Rivera <saulriveraal@icloud.com>
2020-04-04 13:53:17 -0600

    Destiny Complete

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
+//  Created by Saul Rivera on 08/08/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let choice = sender.currentTitle!
+        
+        storyBrain.nextStory(userChoice: choice)
+        updateUI()
+    }
+    
+    private func updateUI() {
+        storyLabel.text = storyBrain.currentStory.title
+        choice1Button.setTitle(storyBrain.currentStory.choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.currentStory.choice2, for: .normal)
+    }
 }
 
--- Destini-iOS13/SceneDelegate.swift
+++ Destini-iOS13/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
+//  Created by Saul Rivera on 08/08/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
